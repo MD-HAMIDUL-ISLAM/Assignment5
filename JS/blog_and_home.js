6c1539fadf0995d02731c05873e89898bab51964
MD-HAMIDUL-ISLAM <hamidulislamcu@gmail.com>
2024-09-24 17:48:29 +0600

    page navigation added

---
+++ JS/blog_and_home.js
@@ -0,0 +1,17 @@
+   document.addEventListener('DOMContentLoaded', function(){
+    const btnBlog = document.getElementById('btn-blog');
+    if(btnBlog) {
+        btnBlog.addEventListener('click', function(event){
+            event.preventDefault();
+            window.location.href = '/blog.html'
+            });
+        }
+        
+        const btnHome = document.getElementById('btn-home');
+    if(btnHome) {
+        btnHome.addEventListener('click', function(event){
+            event.preventDefault();
+            window.location.href = '/index.html'
+            });
+        }
+   })
